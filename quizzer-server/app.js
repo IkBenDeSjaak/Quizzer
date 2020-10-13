e8a788318b10c32d1bcbefbceaccc338bff2f475
Rowan-Paul <rpflynn80@gmail.com>
2020-10-13 15:28:13 +0200

    Add quizzer-server
    With mongoose models, routes, seed files

---
+++ quizzer-server/app.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const categories = require('./routes/categories')
+
+const app = express();
+const port = 3000;
+const dbName = 'quizzer';
+
+app.use(bodyParser.json());
+app.use('/categories', categories)
+
+app.get("/", (req, res) => {
+  res.send("Welcome to quizzer-server");
+});
+
+app.listen(port, () => {
+  console.log(`quizzer-server listening at http://localhost:${port}`);
+  mongoose.connect(`mongodb://localhost:27017/${dbName}`,  {useNewUrlParser: true }, () => {
+      console.log(`quizzer db started on port ${port}`);
+  });
+});
---
+++ quizzer-server/routes/categories.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const express = require('express')
+const router = express.Router()
+
+const questions = require("../models/questions.js")
+const Question = mongoose.model("Questions")
+const dbName = "quizzer";
+
+// middleware that is specific to this router
+router.use(function (req, res, next) {
+  console.log("Entered /categories")
+  next()
+})
+
+// routers
+router.get('/', function (req, res) {
+  Question.distinct('category')
+  .then((categories) => {
+    res.send(categories)
+  })
+})
+
+router.get('/:category/questions/', function (req, res) {
+  const reqCategory = req.params.category
+
+  Question.find({category: reqCategory},{question: 1, answer: 1})
+    .then((questions) => {
+      res.send(questions)
+    })
+})
+
+router.get('/:category/questions/:questionid', function (req, res) {
+  const reqCategory = req.params.category
+  const reqQuestionid = req.params.questionid
+
+  Question.findOne({category: reqCategory, _id: reqQuestionid}, {question: 1, answer: 1})
+    .then((question) => {
+      res.send(question)
+    })
+})
+
+module.exports = router
