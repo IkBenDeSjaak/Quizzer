1ad675a551cfe9b5712a6b180da86474dd1b7cfe
Rowan-Paul <rpflynn80@gmail.com>
2020-11-03 18:41:44 +0100

    Fix teams with no correct questions

--- quizzer-clients/scoreboard-app/src/components/shared/TeamResult.js
+++ quizzer-clients/scoreboard-app/src/components/shared/TeamResult.js
@@ -3,21 +3,26 @@ import React from "react";
 export class TeamResult extends React.Component {
   render() {
     var span = [];
-    if (this.props.rounds) {
-      for (let i = 0; i < this.props.roundAmount; i++) {
-        let points;
-        if (this.props.rounds === "zero") {
-          points = 0;
-        } else {
-          points = this.props.rounds[i];
-        }
 
-        span.push(
-          <span key={this.props.name + " round " + i} className="round">
-            Round {i + 1}: <br></br> {points} questions <br></br> correct
-          </span>
-        );
+    for (let i = 0; i < this.props.roundAmount; i++) {
+      let points;
+      // if the team has zero points, explicitlu say so
+      if (this.props.rounds === undefined || this.props.rounds === null) {
+        points = 0;
+      } else if (
+        this.props.rounds[i] === null ||
+        this.props.rounds[i] === undefined
+      ) {
+        points = 0;
+      } else {
+        points = this.props.rounds[i];
       }
+
+      span.push(
+        <span key={this.props.name + " round " + i} className="round">
+          Round {i + 1}: <br></br> {points} questions <br></br> correct
+        </span>
+      );
     }
 
     return (
